00f2d8cc375127b4456e15382d8a78818f90f093
Andreas Tiefenthaler <at@an-ti.eu>
2013-12-04 00:16:04 +0100

    webapp set environment to production

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,8 @@ require_relative 'worker'
 
 require 'sinatra'
 
+set :environment, :production unless ENV['development']
+
 get '/:task/:cost' do |task, cost|
   Worker.perform_async(task, cost)
 end
