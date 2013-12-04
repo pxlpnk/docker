f1ccdea369a22e050610844a37c5cc935454886c
Andreas Tiefenthaler <at@an-ti.eu>
2013-12-04 17:09:00 +0100

    expose the right ports, remove setup.sh

--- app/docker/containers/run.sh
+++ app/docker/containers/run.sh
@@ -3,6 +3,5 @@
 docker run -d -name redis01 redis
 
 docker run -d -link redis01:db -name worker01 worker
-docker run -p 127.0.0.1::5100 -d -link redis01:db -name app01 webapp
-
+docker run -p 80:5100 -d -link redis01:db -name app01 webapp
 #docker run -d -link redis01:db -name app01 webapp
--- app/docker/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-# docker pull jonsharratt/rails
-
-for i in $( ls ); do
-    (cd "$i" && bash build.sh)
-    cd ..
-done
