cde95fab156f6fd32f3cabde2ecbdf81e0f8ceea
Andreas Tiefenthaler <at@an-ti.eu>
2013-12-05 17:17:41 +0100

    preparing second worker

--- app/docker/containers/run.sh
+++ app/docker/containers/run.sh
@@ -4,4 +4,5 @@ docker run -d -name redis01 redis
 
 docker run -d -link redis01:db -name worker01 worker
 docker run -p 80:5100 -d -link redis01:db -name app01 webapp
-#docker run -d -link redis01:db -name app01 webapp
+
+# docker run -d -link redis01:db -name worker02 worker
